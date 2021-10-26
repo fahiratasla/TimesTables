a1fa2130ea28e5dea9330f85adb62e51199f78c1
Fahira <fahiratasla13@gmail.com>
2021-10-26 12:24:22 +0700

    first commit

---
+++ app/src/main/java/com/example/times_table/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.times_table;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView timesTablesListView;
+
+    public void generateTimesTable(int timesTablesNumber) {
+        ArrayList<String> timesTableContent = new ArrayList<String>();
+
+        for (int j = 1; j <= 10; j++) {
+            timesTableContent.add(Integer.toString(j * timesTablesNumber));
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, timesTableContent);
+
+        timesTablesListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SeekBar timesTablesSeekBar = findViewById(R.id.timesTablesSeekBar);
+        timesTablesListView = findViewById(R.id.timesTablesListView);
+
+        timesTablesSeekBar.setMax(20);
+        timesTablesSeekBar.setProgress(10);
+
+        timesTablesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = 1;
+                int timesTableNumber;
+
+                if (i < min) {
+                   timesTableNumber = min;
+                   timesTablesSeekBar.setProgress(min);
+                } else {
+                    timesTableNumber = i;
+                }
+
+                Log.i("Seekbar Value", Integer.toString(timesTableNumber));
+
+                generateTimesTable(timesTableNumber);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
